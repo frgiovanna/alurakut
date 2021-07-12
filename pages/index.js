66cc61a187cb688db57cf23e76bdd082ea056d7b
Giovanna Freitas <giovannacnf@gmail.com>
2021-07-12 20:04:52 -0300

    :sparkles: add motivational phrase

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,55 @@
-import MainGrid from '../src/components/MainGrid'
-import Box from '../src/components/Box'
-import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
-import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
+import MainGrid from "../src/components/MainGrid";
+import Box from "../src/components/Box";
+import {
+  AlurakutMenu,
+  OrkutNostalgicIconSet,
+} from "../src/lib/AlurakutCommons";
+import { ProfileRelationsBoxWrapper } from "../src/components/ProfileRelations";
 
 function ProfileSidebar(props) {
   console.log(props);
   return (
     <Box>
-      <img src={`https://github.com/${props.githubUser}.png`} style={{ borderRadius: '8px' }} />
+      <img
+        src={`https://github.com/${props.githubUser}.png`}
+        style={{ borderRadius: "8px" }}
+      />
     </Box>
-  )
+  );
 }
 
 export default function Home() {
-  const user = 'frgiovanna';
-  const friends = [
-    'laura-martins',
-    'arthurMSA',
-    'FagnerRumenigg',
-  ]
+  const user = "frgiovanna";
+  const friends = ["laura-martins", "arthurMSA", "FagnerRumenigg"];
+
+  var motivationalPhrases = [
+    "Quem se apaixona por si mesmo não tem rivais",
+    "A sorte vem sempre ao seu encontro",
+    "Um dia sem sorrisos é um dia perdido",
+    "O homem procura a paz,mas ao mesmo tempo anseia pela guerra.",
+  ];
+  var randomPhrase =
+    motivationalPhrases[Math.floor(Math.random() * motivationalPhrases.length)];
 
   return (
     <>
       <AlurakutMenu />
       <MainGrid>
         {/* <Box style="grid-area: profileArea;"> */}
-        <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+        <div className="profileArea" style={{ gridArea: "profileArea" }}>
           <ProfileSidebar githubUser={user} />
         </div>
-        <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+        <div className="welcomeArea" style={{ gridArea: "welcomeArea" }}>
           <Box>
-            <h1 className="title">
-              Bem vindo(a), {user} 
-            </h1>
-
+            <h1 className="title">Bem vindo(a), {user}</h1>
+            <p style={{ color: "#999999" }}><b>Sorte do dia: </b>{randomPhrase}</p>
             <OrkutNostalgicIconSet />
           </Box>
         </div>
-        <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+        <div
+          className="profileRelationsArea"
+          style={{ gridArea: "profileRelationsArea" }}
+        >
           <ProfileRelationsBoxWrapper>
             <h2 className="smallTitle">
               Pessoas da comunidade ({friends.length})
@@ -52,12 +64,12 @@ export default function Home() {
                       <span>{item}</span>
                     </a>
                   </li>
-                )
+                );
               })}
             </ul>
           </ProfileRelationsBoxWrapper>
         </div>
       </MainGrid>
     </>
-  )
+  );
 }
